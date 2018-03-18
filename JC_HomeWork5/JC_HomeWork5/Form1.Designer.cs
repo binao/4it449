082a9ba948989ad03085af99f4ac046016c7dfbe
johnyBlacker <johnyblacker1@gmail.com>
2018-03-18 21:17:29 +0100

    Create new homework - From app

---
+++ JC_HomeWork5/JC_HomeWork5/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace JC_HomeWork5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textCustomerBox = new System.Windows.Forms.TextBox();
+            this.orderDataGridView = new System.Windows.Forms.DataGridView();
+            this.orderIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orderDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orderDOBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.loadingLabel = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.orderDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderDOBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(688, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClickAsync);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customer ID (GODOS, SUPRD):";
+            // 
+            // textCustomerBox
+            // 
+            this.textCustomerBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textCustomerBox.Location = new System.Drawing.Point(225, 20);
+            this.textCustomerBox.Name = "textCustomerBox";
+            this.textCustomerBox.Size = new System.Drawing.Size(441, 22);
+            this.textCustomerBox.TabIndex = 2;
+            // 
+            // orderDataGridView
+            // 
+            this.orderDataGridView.AllowUserToAddRows = false;
+            this.orderDataGridView.AllowUserToDeleteRows = false;
+            this.orderDataGridView.AllowUserToResizeColumns = false;
+            this.orderDataGridView.AllowUserToResizeRows = false;
+            this.orderDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.orderDataGridView.AutoGenerateColumns = false;
+            this.orderDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.orderDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.orderDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.orderIDDataGridViewTextBoxColumn,
+            this.orderDateDataGridViewTextBoxColumn,
+            this.LastName});
+            this.orderDataGridView.DataSource = this.orderDOBindingSource;
+            this.orderDataGridView.Location = new System.Drawing.Point(15, 49);
+            this.orderDataGridView.Name = "orderDataGridView";
+            this.orderDataGridView.RowTemplate.Height = 24;
+            this.orderDataGridView.Size = new System.Drawing.Size(752, 264);
+            this.orderDataGridView.TabIndex = 3;
+            this.orderDataGridView.Visible = false;
+            // 
+            // orderIDDataGridViewTextBoxColumn
+            // 
+            this.orderIDDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.orderIDDataGridViewTextBoxColumn.DataPropertyName = "OrderID";
+            this.orderIDDataGridViewTextBoxColumn.HeaderText = "OrderID";
+            this.orderIDDataGridViewTextBoxColumn.Name = "orderIDDataGridViewTextBoxColumn";
+            this.orderIDDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // orderDateDataGridViewTextBoxColumn
+            // 
+            this.orderDateDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.orderDateDataGridViewTextBoxColumn.DataPropertyName = "OrderDate";
+            this.orderDateDataGridViewTextBoxColumn.HeaderText = "OrderDate";
+            this.orderDateDataGridViewTextBoxColumn.Name = "orderDateDataGridViewTextBoxColumn";
+            this.orderDateDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // LastName
+            // 
+            this.LastName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.LastName.DataPropertyName = "LastName";
+            this.LastName.HeaderText = "EmployeeLastName";
+            this.LastName.Name = "LastName";
+            this.LastName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // orderDOBindingSource
+            // 
+            this.orderDOBindingSource.DataSource = typeof(JC_HomeWork5.DataObjects.OrderDO);
+            // 
+            // loadingLabel
+            // 
+            this.loadingLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loadingLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.loadingLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.loadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.loadingLabel.Location = new System.Drawing.Point(133, 152);
+            this.loadingLabel.Multiline = true;
+            this.loadingLabel.Name = "loadingLabel";
+            this.loadingLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.loadingLabel.Size = new System.Drawing.Size(549, 46);
+            this.loadingLabel.TabIndex = 4;
+            this.loadingLabel.Text = "Loading, please wait...";
+            this.loadingLabel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.loadingLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 331);
+            this.Controls.Add(this.loadingLabel);
+            this.Controls.Add(this.orderDataGridView);
+            this.Controls.Add(this.textCustomerBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(797, 378);
+            this.Name = "Form1";
+            this.Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)(this.orderDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderDOBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textCustomerBox;
+        private System.Windows.Forms.DataGridView orderDataGridView;
+        private System.Windows.Forms.BindingSource orderDOBindingSource;
+        private System.Windows.Forms.TextBox loadingLabel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn orderIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn orderDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+    }
+}
+
