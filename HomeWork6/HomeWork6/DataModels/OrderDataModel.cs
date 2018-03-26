722aab44110a701375efd1e920d5e141ade417d9
Ondrej Bina <binao@seznam.cz>
2018-03-26 16:04:06 -0700

    Add HomeWork6

---
+++ HomeWork6/HomeWork6/DataModels/OrderDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6.DataModels
+{
+    class OrderDataModel
+    {
+        public int OrderID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string EmployeeLastName { get; set; }
+        public string CustomerID { get; set; }
+    }
+}
