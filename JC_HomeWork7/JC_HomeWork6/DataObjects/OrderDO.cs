e3e9afbe6835b56cc8b0a0209c183d701f1c97d2
johnyBlacker <johnyblacker1@gmail.com>
2018-03-27 13:56:58 +0200

    Add homework 7 - Focus, format of date, mouse events, default style

---
+++ JC_HomeWork7/JC_HomeWork6/DataObjects/OrderDO.cs
@@ -0,0 +1,54 @@
+﻿using JC_HomeWork6.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JC_HomeWork6.DataObjects
+{
+    class OrderDO
+    {
+        public int orderID { get; set; }
+        public Nullable<System.DateTime> orderDate { get; set; }
+        public string lastName { get; set; }
+        public string customerID { get; set; }
+
+        //Create orders async without parametrs
+        public static async Task<List<OrderDO>> GetOrdersAsync()
+        {
+            using (MyCSharpDotNetEntities context =
+                new MyCSharpDotNetEntities())
+            {
+                return await context.Orders
+                    .Select(x => new OrderDO()
+                    {
+                        orderID = x.OrderID,
+                        orderDate = x.OrderDate,
+                        lastName = x.Employee.LastName
+                    })
+                    .ToListAsync();
+            }
+        }
+
+        //Create orders async with parametr customerID
+        public static async Task<List<OrderDO>> GetOrdersAsync(string customerID)
+        {
+            using (MyCSharpDotNetEntities context =
+                new MyCSharpDotNetEntities())
+            {
+                return await context.Orders
+                    .Select(x => new OrderDO()
+                    {
+                        orderID = x.OrderID,
+                        orderDate = x.OrderDate,
+                        lastName = x.Employee.LastName,
+                        customerID = x.CustomerID
+                    })
+                    .Where(x => x.customerID.Equals(customerID))
+                    .ToListAsync();
+            }
+        }
+    }
+}
