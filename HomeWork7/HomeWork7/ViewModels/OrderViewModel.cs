ff08769cb2633d095c032122fd95513435aeafa6
Ondrej Bina <binao@seznam.cz>
2018-04-02 14:11:44 -0700

    Add HomeWork7

---
+++ HomeWork7/HomeWork7/ViewModels/OrderViewModel.cs
@@ -0,0 +1,89 @@
+﻿using HomeWork7.Commands;
+using HomeWork7.DataModels;
+using HomeWork7.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HomeWork7.ViewModels
+{
+    class OrderViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<OrderDataModel> _Orders;
+        private String _SearchText;
+        private bool _Loading = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+  
+        public ICommand LoadCommand { get; private set; }
+
+        public ObservableCollection<OrderDataModel> Orders
+        {
+            get { return _Orders; }
+            set {
+                _Orders = value;
+                OnPropertyChanged("Orders");
+            }
+        }
+
+        public bool Loading
+        {
+            get { return _Loading; }
+            set
+            {
+                _Loading = value;
+                OnPropertyChanged("Loading");
+            }
+        }
+
+        public String SearchText
+        {
+            get { return _SearchText; }
+            set
+            {
+                _SearchText = value;
+                OnPropertyChanged("SearchText");
+            }
+        }
+
+        public OrderViewModel()
+        {
+            //Create command for orders loading
+            LoadCommand = new Command(obj => true, async obj => await LoadOrdersAsync());
+        }
+
+        public async Task<ObservableCollection<OrderDataModel>> LoadOrdersAsync()
+        {
+            using (MyCSharpDotNetEntities context =
+                new MyCSharpDotNetEntities())
+            {
+                Loading = true;
+                await Task.Delay(1000);
+
+                Orders = new ObservableCollection<OrderDataModel>( context.Orders
+                    .Where(order => order.CustomerID == SearchText)
+                    .Select(order => new OrderDataModel
+                    {
+                        OrderID = order.OrderID,
+                        OrderDate = order.OrderDate,
+                        EmployeeLastName = order.Employee.LastName,
+                        CustomerID = order.CustomerID
+                    })) ;
+                Loading = false;
+                return await Task.FromResult(Orders);
+            }
+        }
+
+        //Notify about property change
+        private void OnPropertyChanged(String info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
