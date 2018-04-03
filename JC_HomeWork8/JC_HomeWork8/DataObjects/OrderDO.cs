d268bf3ef926daaaf3289eb1351b549df4abbad9
johnyBlacker <johnyblacker1@gmail.com>
2018-04-03 15:48:28 +0200

    Add homework8

---
+++ JC_HomeWork8/JC_HomeWork8/DataObjects/OrderDO.cs
@@ -0,0 +1,54 @@
+﻿using JC_HomeWork8.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JC_HomeWork6.DataObjects
+{
+    class OrderDO
+    {
+        public int orderID { get; set; }
+        public Nullable<System.DateTime> orderDate { get; set; }
+        public string lastName { get; set; }
+        public string customerID { get; set; }
+
+        //Create orders without parametrs
+        public static List<OrderDO> GetOrders()
+        {
+            using (MyCSharpDotNetEntities context =
+                new MyCSharpDotNetEntities())
+            {
+                return context.Orders
+                    .Select(x => new OrderDO
+                    {
+                        orderID = x.OrderID,
+                        orderDate = x.OrderDate,
+                        lastName = x.Employee.LastName
+                    }).ToList();
+ 
+            }
+        }
+
+        //Create orders with parametr customerID
+        public static List<OrderDO> GetOrders(string customerID)
+        {
+            using (MyCSharpDotNetEntities context =
+                new MyCSharpDotNetEntities())
+            {
+                return  context.Orders
+                    .Select(x => new OrderDO()
+                    {
+                        orderID = x.OrderID,
+                        orderDate = x.OrderDate,
+                        lastName = x.Employee.LastName,
+                        customerID = x.CustomerID
+                    })
+                    .Where(x => x.customerID.Equals(customerID))
+                    .ToList();
+            }
+        }
+    }
+}
---
+++ JC_HomeWork8/JC_HomeWork8/Default.aspx.designer.cs
@@ -0,0 +1,69 @@
+﻿//------------------------------------------------------------------------------
+// <automaticky generován>
+//     Tento kód vygeneroval nástroj.
+//
+//     Změny tohoto souboru mohou způsobit nesprávné chování a budou ztraceny, jestliže
+//     kód je znovu vygenerován. 
+// </automaticky generován>
+//------------------------------------------------------------------------------
+
+namespace JC_HomeWork8 {
+    
+    
+    public partial class Default {
+        
+        /// <summary>
+        /// ovládací prvek form1.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// ovládací prvek NameLabel.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label NameLabel;
+        
+        /// <summary>
+        /// ovládací prvek ContentTextBox.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ContentTextBox;
+        
+        /// <summary>
+        /// ovládací prvek LoadButton.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button LoadButton;
+        
+        /// <summary>
+        /// ovládací prvek OrderDataView.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView OrderDataView;
+        
+        /// <summary>
+        /// ovládací prvek DataLabel.
+        /// </summary>
+        /// <remarks>
+        /// Automaticky generované pole.
+        /// Pro úpravy přesuňte pole deklarace ze souboru návrháře do souboru kódu na pozadí.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label DataLabel;
+    }
+}
