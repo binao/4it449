e7078463be97b529f02085edd0c6bb7cec1262fa
Ondrej Bina <binao@seznam.cz>
2018-04-16 12:48:41 -0700

    Add HomeWork9

---
+++ HomeWork9/HomeWork9/Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HomeWork9.Models;
+
+namespace HomeWork9.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private MyCSharpDotNetEntities db = new MyCSharpDotNetEntities();
+
+        // GET: Orders
+        public async Task<ActionResult> Index(String CustomerId)
+        {
+            var orders = db.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Employee)
+                .Where(order => order.CustomerID == CustomerId);
+
+            return View(await orders.ToListAsync());
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HomeWork9/HomeWork9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomeWork9.MvcApplication" Language="C#" %>
