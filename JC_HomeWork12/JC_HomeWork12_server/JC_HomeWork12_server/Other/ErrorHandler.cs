0b5a56cf491929ffba562e09c5d17339b5a580ba
johnyBlacker <johnyblacker1@gmail.com>
2018-04-28 14:45:43 +0200

    Add_Homework12

---
+++ JC_HomeWork12/JC_HomeWork12_server/JC_HomeWork12_server/Other/ErrorHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Web.Http.ExceptionHandling;
+
+namespace JC_HomeWork12_server.Other
+{
+    public class ErrorHandler : ExceptionLogger
+    {
+        //Error handlerer
+        public override void Log(ExceptionLoggerContext context)
+        {
+            if (Debugger.IsAttached)
+            {
+                throw context.Exception;
+            }
+            else
+            {
+                // Log the exception here
+            }
+        }
+    }
+}
